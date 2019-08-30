24f77466762f1bb19ef3e98ed9e37fe6194cec05
Benjamin Atkin <ben@benatkin.com>
2019-08-30 02:22:37 -0400

    refactor cli a bit

--- components/commands/index.js
+++ components/commands/index.js
@@ -4,34 +4,46 @@ import Docs from './docs'
 const giphy = new Giphy()
 const docs = new Docs()
 
-export default async (message) => {
-  let messages = [
-    {
-      type: 'input',
-      text: message,
-    }
-  ]
-  if (message === 'help') {
-    messages.push({
+const inputMessage = message => ({
+  type: 'input',
+  text: message,
+})
+
+const commands = {
+  help({message}) {
+    const outputMessage = {
       type: 'help',
-    })
-  } else if (message.startsWith('docs ')) {
-    const args = message.split(/\s+/)
-    const url = args[1]
-    const outputMessages = docs.run(args[1])
-    messages = [...messages, ...outputMessages]
-  } else if (message.startsWith('giphy ')) {
-    const args = message.split(/\s+/)
-    const outputMessage = await giphy.run(...args.slice(1))
-    messages.push(outputMessage)
-  } else if (message.startsWith('note ')) {
-    messages[0].content = <div className="input-message">note</div>
-    messages = [{type: 'input', text: 'note'}, {type: 'text', text: message.replace(/^note\s*/, '')}]
-  } else {
-    messages.push({
-      type: 'output',
-      content: <p>Command not found.</p>
-    })
+    }
+    return [inputMessage(message), outputMessage]
+  },
+  docs({message, args}) {
+    const url = args[0]
+    const outputMessages = docs.run(url)
+    return [inputMessage(message), ...outputMessages]
+  },
+  async giphy({message, args}) {
+    const outputMessage = await giphy.run(...args)
+    return [inputMessage(message), outputMessage]
+  },
+  note({message}) {
+    return [
+      {type: 'input', text: 'note'},
+      {type: 'text', text: message.replace(/^note\s*/, '')}
+    ]
   }
-  return messages
+}
+
+export default async (message) => {
+  const allArgs = message.split(/\s+/)
+  const command = allArgs[0]
+  const args = allArgs.slice(1)
+  const commandFn = commands[command]
+  if (commandFn) {
+    return commandFn({command, args, message})
+  } else {
+    return [
+      inputMessage(message),
+      { type: 'output', content: <p>Command not found.</p> }
+    ]
+  }
 }
