75457e7b00cb8869304ac092169d0abc694aea65
Benjamin Atkin <ben@benatkin.com>
2020-05-19 12:20:45 -0400

    begin app api with typescript

---
+++ packages/api-finder/app.ts
@@ -0,0 +1,51 @@
+function run({
+  action,
+  params: { url },
+}: {
+  action: string
+  params: { url: string }
+}) {
+  return {}
+}
+
+interface App {
+  name: string
+  provides: {
+    [key: string]: {
+      path: string[]
+      actions: {
+        [key: string]: {
+          args: string[]
+          autorun: {
+            type: string
+            host: string
+            path: string
+          }
+        }
+      }
+    }
+  }
+  run(this: App, props: { action: string; params: object }): object
+}
+
+export default async function app(): Promise<App> {
+  return {
+    name: 'API finder',
+    provides: {
+      apiFinder: {
+        path: [],
+        actions: {
+          lookup: {
+            args: ['url'],
+            autorun: {
+              type: 'url',
+              host: '*',
+              path: '*',
+            },
+          },
+        },
+      },
+    },
+    run,
+  }
+}
