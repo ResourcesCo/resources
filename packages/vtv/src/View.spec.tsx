42df345adf9a3e62a95065b6987178f160c57940
Benjamin Atkin <ben@resources.co>
2020-10-04 23:55:27 -0400

    look for text

--- packages/vtv/src/View.spec.tsx
+++ packages/vtv/src/View.spec.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import View from "./View";
 
 describe("View", () => {
-  it("should render successfully", () => {
+  it("should render with dark theme", () => {
     const { container } = render(
       <View
         name="root"
@@ -18,5 +18,37 @@ describe("View", () => {
     );
     expect(container.querySelector('.vtv-theme-dark')).toBeTruthy();
     expect(container.querySelector('.vtv-theme-light')).toBeFalsy();
+  });
+
+  it("should render with light theme", () => {
+    const { container } = render(
+      <View
+        name="root"
+        onChange={() => {}}
+        onAction={() => {}}
+        onPickId={() => {}}
+        value={{}}
+        state={{}}
+        theme="light"
+      />
+    );
+    expect(container.querySelector('.vtv-theme-light')).toBeTruthy();
+    expect(container.querySelector('.vtv-theme-dark')).toBeFalsy();
+  });
+
+  it("should contain text", () => {
+    const { baseElement } = render(
+      <View
+        name="root"
+        onChange={() => {}}
+        onAction={() => {}}
+        onPickId={() => {}}
+        value={'hello'}
+        state={{}}
+        theme="dark"
+      />
+    );
+    const el = screen.getByText('hello');
+    expect(el).toBeTruthy();
   });
 });
