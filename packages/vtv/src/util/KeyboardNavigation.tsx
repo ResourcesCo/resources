7352a47152e5f8a10034f388349824507af2e8b1
Benjamin Atkin <ben@resources.co>
2022-09-29 23:54:33 -0700

    implement moving up and down, and expanding/collapsing with keyboard

--- packages/vtv/src/util/KeyboardNavigation.tsx
+++ packages/vtv/src/util/KeyboardNavigation.tsx
@@ -1,10 +1,64 @@
 import React from 'react'
 
+const ROOT_CLASS = 'vtv--view'
 const NODE_CLASS = 'vtv--node-view--row'
 const NODE_CHILDREN_CLASS = 'vtv--node-view--children'
 const NODE_NAME_CLASS = 'vtv--name-button'
+const NODE_EXPAND_CLASS = 'vtv--expand-button'
 const CODE_VIEW_CLASS = 'vtv--code-view--textarea-wrapper'
 const MAX_DEPTH = 20
+
+function isVisible(el: Element) {
+  if (el instanceof HTMLElement) {
+    return el.offsetParent !== null
+  } else {
+    return false
+  }
+}
+
+function findNode(root: HTMLElement, node: Element, direction: string): HTMLElement | undefined {
+  let parent = node
+  for (let parentDepth = 0; parentDepth < MAX_DEPTH; parentDepth++) {
+    const nodes = [...parent.querySelectorAll(`.${NODE_CLASS}`)]
+    if (direction === 'ArrowUp') {
+      nodes.reverse()
+    }
+    let returnNext = false
+    for (let i = 0; i < nodes.length; i++) {
+      const currentNode = nodes[i]
+      if (returnNext) {
+        if (isVisible(currentNode) && currentNode instanceof HTMLElement) {
+          return currentNode
+        }
+      }
+      if (currentNode === node) {
+        returnNext = true
+      }
+    }
+    if (parent === root) {
+      break;
+    }
+    parent = parent.parentElement
+    if (!(parent instanceof HTMLElement)) {
+      break;
+    }
+  }
+  return undefined
+}
+
+function navigate(root: HTMLElement, node: HTMLElement, direction: string): HTMLElement | undefined {
+  if (direction === 'ArrowUp' || direction === 'ArrowDown') {
+    const nodeContainer = node.closest(`.${NODE_CLASS}`)
+    const navNode = findNode(root, nodeContainer, direction)
+    if (navNode) {
+      const nodeNameEl = navNode.querySelector(`.${NODE_NAME_CLASS}`)
+      if (nodeNameEl instanceof HTMLElement) {
+        nodeNameEl.focus()
+      }
+    }
+  }
+  return undefined
+}
 
 const KeyboardNavigation = React.forwardRef(({themeClass, children}, ref) => {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
@@ -12,6 +66,7 @@ const KeyboardNavigation = React.forwardRef(({themeClass, children}, ref) => {
     let node = target
     let arrowNavigation = true
     let treeNavigation = false
+    let root = target.closest(ROOT_CLASS) as HTMLElement
     for (let i=0; i < MAX_DEPTH; i++) {
       if (
         node &&
@@ -37,26 +92,27 @@ const KeyboardNavigation = React.forwardRef(({themeClass, children}, ref) => {
       )
     ) {
       if (arrowNavigation) {
-        if (e.code === 'ArrowUp') {
+        if (e.code === 'ArrowUp' || e.code === 'ArrowDown') {
           e.preventDefault()
-          console.log('up arrow')
-        } else if (e.code === 'ArrowDown') {
-          e.preventDefault()
-          console.log('down arrow')
+          navigate(root, node, e.code)
         } else if (treeNavigation) {
-          if (e.code === 'ArrowLeft') {
-            e.preventDefault()
-            console.log('left arrow')
-          } else if (e.code === 'ArrowRight') {
-            e.preventDefault()
-            console.log('right arrow')
+          if (e.code === 'Space') {
+            const nodeContainer = node.closest(`.${NODE_CLASS}`)
+            if (nodeContainer instanceof HTMLElement) {
+              const expandButton = nodeContainer.querySelector(`.${NODE_EXPAND_CLASS} button`)
+              if (expandButton instanceof HTMLElement) {
+                console.log({nodeContainer, expandButton})
+                const event = new Event('click')
+                expandButton.dispatchEvent(event)
+                expandButton.click()
+              }
+            }
           }
+          // TODO: open menu on enter - menu needs keyboard too
         }
       } else {
         if (e.code === 'Escape') {
-          e.preventDefault()
-          console.log('escape')
-          // TODO: focus node containing code view (sibling of NODE_CHILDREN_CLASS)
+          // TODO: if in code view, navigate to parent element
         }
       }
     }
